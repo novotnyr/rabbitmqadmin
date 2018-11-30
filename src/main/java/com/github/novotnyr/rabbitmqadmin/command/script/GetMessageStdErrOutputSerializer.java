85a22c4c54858a70b1d7605cf6a7bae10bd24055
Robert Novotny <novotnr0@gmail.com>
2018-11-30 22:01:01 +0100

    Implement pretty serializer for GetMessage

---
+++ src/main/java/com/github/novotnyr/rabbitmqadmin/command/script/GetMessageStdErrOutputSerializer.java
@@ -0,0 +1,28 @@
+package com.github.novotnyr.rabbitmqadmin.command.script;
+
+import com.github.novotnyr.rabbitmqadmin.command.RetrievedMessage;
+import com.github.novotnyr.rabbitmqadmin.log.StdErr;
+
+import java.util.List;
+
+public class GetMessageStdErrOutputSerializer implements ScriptOutputSerializer<List<RetrievedMessage>> {
+    private final StdErr stdErr;
+
+    public GetMessageStdErrOutputSerializer(StdErr stdErr) {
+        this.stdErr = stdErr;
+    }
+
+    @Override
+    public void serialize(Class<?> commandClass, List<RetrievedMessage> retrievedMessages) {
+        for (RetrievedMessage message : retrievedMessages) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Messages left in Queue: ").append(message.getMessagesLeftInQueue()).append("\n");
+            sb.append("Payload Size: ").append(message.getPayloadSize()).append("\n");
+            sb.append("Redelivered: ").append(message.isRedelivered() ? "yes" : "no").append("\n");
+            sb.append("Exchange: ").append(message.getExchange()).append("\n");
+            sb.append("Payload: ").append(message.getPayload());
+
+            stdErr.println(sb.toString());
+        }
+    }
+}
